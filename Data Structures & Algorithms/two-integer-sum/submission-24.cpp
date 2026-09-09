e0497c70157ba64ef40b072affc4916ba640b812
Jackson Thomas <jacksonthomas0648@gmail.com>
2026-09-09 01:28:36 -0500

    Add: two-integer-sum - submission-24

---
+++ Data Structures & Algorithms/two-integer-sum/submission-24.cpp
@@ -0,0 +1,6 @@
+class Solution {public:vector<int> twoSum(vector<int>& nums, int target) {
+        std::unordered_map<int, int> m;
+        for (int i = 0; i < nums.size(); i++) {
+            int x=target-nums[i];
+            if (m.contains(x)) {return {m[x], i};}
+            m[nums[i]] = i;} return {};}};
