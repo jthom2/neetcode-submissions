10f7e2aba886570892950ac40500631bf4d2d62c
Jackson Thomas <jacksonthomas0648@gmail.com>
2026-09-09 01:21:42 -0500

    Add: two-integer-sum - submission-17

---
+++ Data Structures & Algorithms/two-integer-sum/submission-17.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        std::unordered_map<int, int> diffs;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int diff = target - nums[i];
+
+            if (diffs.contains(diff)) {
+                // vector<int> indexes = {diffs[diff], i};
+                // return indexes;
+                return {diffs[diff], i};
+            }
+            
+            diffs[nums[i]] = i;
+            
+        }
+
+    
+
+      return {-1};
+    }
+};
