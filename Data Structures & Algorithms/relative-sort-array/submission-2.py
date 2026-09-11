5633ebd8d4411ba138065ef523033301eb7d372f
Jackson Thomas <jacksonthomas0648@gmail.com>
2026-09-11 18:54:02 -0500

    Add: relative-sort-array - submission-2

---
+++ Data Structures & Algorithms/relative-sort-array/submission-2.py
@@ -0,0 +1,24 @@
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+
+        b = set(arr2)   ;   mp = {}       ; res = []
+
+        extra = []
+
+        for n in arr1:
+            if n not in b: extra.append(n)
+            elif n not in mp: mp[n] = 1; continue
+            elif n in mp: mp[n] += 1; continue
+
+        for n in arr2:
+            if n in mp:
+                for i in range(mp[n]): res.append(n)
+
+
+        extra.sort()
+
+
+        return res + extra
+
+
+        
