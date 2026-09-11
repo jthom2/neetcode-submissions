02d7bdc3fddd36ee3fa5421d1fd8ad4d63bdd06a
Jackson Thomas <jacksonthomas0648@gmail.com>
2026-09-11 14:43:51 -0500

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.py
@@ -0,0 +1,22 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = []; d = {}; strs.sort()
+
+        lst= strs.copy(); lst.sort(); lst=list(map(sorted,lst)); lst= list(map("".join, lst))
+
+    
+        for s in lst: d[s] = []
+
+        for i, s in enumerate(lst):
+            if s in d: d[s].append(strs[i])
+
+            
+
+        for k, v in d.items():
+            if len(v) is None: continue
+
+            res.append(v)
+            
+        
+        return res
+        
