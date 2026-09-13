73fbfd00dcef35dabd36ef9948dc48995a743453
Jackson Thomas <jacksonthomas0648@gmail.com>
2026-09-13 10:09:53 -0500

    Add: find-pivot-index - submission-6

---
+++ Data Structures & Algorithms/find-pivot-index/submission-6.py
@@ -0,0 +1,13 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        lth = len(nums); prefixSum = {}; sm = 0; postfixSum = {}; pm = 0; j = lth-1
+
+        for i in range(lth):
+            sm += nums[i]; prefixSum[i] = sm
+            pm += nums[j]; postfixSum[j] = pm
+            j -= 1
+
+        for i in range(lth):
+            if prefixSum[i] == postfixSum[i]: return i
+
+        return -1
