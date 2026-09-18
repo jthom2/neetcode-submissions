8b98f17be69644e617f47859ebd5e5d8e8ad7a4a
Jackson Thomas <jacksonthomas0648@gmail.com>
2026-09-18 12:25:34 -0500

    Add: score-of-a-string - submission-2

---
+++ Data Structures & Algorithms/score-of-a-string/submission-2.py
@@ -0,0 +1,13 @@
+class Solution:
+    def scoreOfString(self, s: str) -> int:
+        sm = 0
+
+        if len(s) < 2: return 0
+
+        for i, c in enumerate(s):
+            if (i == 0): sm += abs(ord(s[i+1]) - ord(c))
+            elif (i == len(s)-1): pass
+            else: sm += abs(ord(s[i+1]) - ord(c))
+    
+        return sm
+        
