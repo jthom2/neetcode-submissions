677f7056020eaec42a0903f2ed45219094e89c8e
Jackson Thomas <jacksonthomas0648@gmail.com>
2026-09-18 15:42:37 -0500

    Add: maximum-ascending-subarray-sum - submission-9

---
+++ Data Structures & Algorithms/maximum-ascending-subarray-sum/submission-9.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxAscendingSum(self, nums: List[int]) -> int:
+        sms = []; sm = 0
+
+        for i in range(1, len(nums)):
+            if nums[i-1] < nums[i]:
+                if sm == 0: sm = nums[i-1] + nums[i]
+                else: sm += nums[i]
+            else:
+                sms.append(sm)
+                sms.append(nums[i-1])
+                sm = nums[i]
+
+        sms.append(sm)
+
+        if sms: return max(sms)
+        else: return 0
